64370d79e5f338dbc362e96dfc997a8f4ecc6cb9
somax <somaxj@163.com>
2017-12-13 23:58:10 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const Auth = require('./lib/auth');
+const Server = require('./lib/server');
+const request = require('request');
+
+
+let app = new Server();
+
+app = new Auth(app)
+
+app.get('/api-list', (req, res) => {
+    request(
+        {
+            url: 'https://docs.teambition.com/all/doc'
+        },
+        (e, r, body) => {
+            res.send(body)
+        }
+    )
+})
+
+/**
+ * 
+ * 接口代理，当然浏览器端也可以直接调用，这里做代理是为了后续数据入库做准备 
+ */
+app.use('/api', (req, res) => {
+    
+    request(
+        {
+            url: `https://api.teambition.com${req.url}`,
+            headers: {
+                Authorization: `OAuth2 ${req.cookies.tb_token}`
+            },
+            json: true
+        },
+        (e, r, body) => {
+            res.send(body)
+        }
+    )
+});
+
+
+
+
